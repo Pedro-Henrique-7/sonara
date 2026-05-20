7d4828347101a6f8828fdc500df68eb875cc1c41
yuri-stcrzwx17 <yurisilva.oliveira0017@gmail.com>
2026-05-20 14:01:57 -0300

    alterações necessarias feitas

--- code/frontend/src/Casa_Show/sobreArtista.jsx
+++ code/frontend/src/Casa_Show/sobreArtista.jsx
@@ -1,88 +1,111 @@
 import "./sobreArtista.css";
+
+import HeaderCasaShow from "./headerCasaShow";
+import FooterSonara from "../Artista/footer";
+
 import {
-  FaFacebook,
+  FaFacebookF,
   FaSpotify,
   FaInstagram,
   FaYoutube,
   FaMapMarkerAlt,
   FaStar,
 } from "react-icons/fa";
-import HeaderCasaShow from "./headerCasaShow.jsx";
-import FooterSonara from "../Artista/footer.jsx";
 
-//import fotoPerfil from "../img/fotoPerfil.jng";
-
-export default function TelaSobreArtista() {
+export default function SobreArtista() {
   return (
-    <div className="visualizarArtistaPagina">
+    <>
       <HeaderCasaShow />
-      <div className="visualizarArtistaContainer">
-        <h2 className="visualizarArtistaTitulo">Ver Artista</h2>
 
-        <section className="visualizarArtistaCard">
-          <div className="visualizarArtistaTopo">
-            <img
-              //src={fotoPerfil}
-              alt="Artista"
-              className="visualizarArtistaImagem"
-            />
+      <main className="sonaraSobreArtistaPagina">
+        <div className="sonaraSobreArtistaContainer">
+          <h1 className="sonaraSobreArtistaTitulo">Ver Artista</h1>
 
-            <div className="visualizarArtistaInfo">
-              <h1 className="visualizarArtistaNome">Pedro Henrique</h1>
+          <div className="sonaraSobreArtistaCard">
+            {/* TOPO */}
+            <div className="sonaraSobreArtistaTopo">
+              <div className="sonaraSobreArtistaFotoArea">
+                <img
+                  src="https://images.unsplash.com/photo-1501386761578-eac5c94b800a?q=80&w=1200&auto=format&fit=crop"
+                  alt="Artista"
+                  className="sonaraSobreArtistaFoto"
+                />
+              </div>
 
-              <p className="visualizarArtistaGenero">Jazz e Clássica</p>
+              <div className="sonaraSobreArtistaInfo">
+                <h2>Pedro Henrique</h2>
 
-              <p className="visualizarArtistaTelefone">(11) 98017-0001</p>
+                <span>Jazz e Clássica</span>
 
-              <div className="visualizarArtistaEstrelas">
-                <FaStar />
-                <FaStar />
-                <FaStar />
-                <FaStar />
-                <FaStar className="visualizarArtistaEstrelaApagada" />
+                <p>(11) 98017-0001</p>
+
+                <div className="sonaraSobreArtistaAvaliacao">
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar className="sonaraSobreArtistaStarOff" />
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="visualizarArtistaLocalizacao">
-            <FaMapMarkerAlt className="visualizarArtistaIconeLocal" />
+            {/* LOCAL */}
+            <div className="sonaraSobreArtistaLocal">
+              <FaMapMarkerAlt className="sonaraSobreArtistaLocalIcone" />
 
-            <p>Jandira, Street Vicent nº12</p>
-          </div>
-
-          <p className="visualizarArtistaDescricao">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
-
-          <div className="visualizarArtistaRodape">
-            <div className="visualizarArtistaCache">
-              <span>Cache Pretendido:</span>
-              <strong>XXXXX</strong>
+              <p>Jandira, Street Vicent nº12</p>
             </div>
 
-            <div className="visualizarArtistaRedes">
-              <FaFacebook />
-              <FaSpotify />
-              <FaInstagram />
-              <FaYoutube />
+            {/* DESCRIÇÃO */}
+            <div className="sonaraSobreArtistaDescricao">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+
+            {/* CACHE + REDES */}
+            <div className="sonaraSobreArtistaRodapeInfo">
+              <div className="sonaraSobreArtistaCache">
+                <h3>Cachê Pretendido:</h3>
+
+                <span>XXXXX</span>
+              </div>
+
+              <div className="sonaraSobreArtistaRedes">
+                <a href="/">
+                  <FaFacebookF />
+                </a>
+
+                <a href="/">
+                  <FaSpotify />
+                </a>
+
+                <a href="/">
+                  <FaInstagram />
+                </a>
+
+                <a href="/">
+                  <FaYoutube />
+                </a>
+              </div>
+            </div>
+
+            {/* BOTÕES */}
+            <div className="sonaraSobreArtistaBotoes">
+              <button className="sonaraSobreArtistaBtn">Reprovar</button>
+
+              <button className="sonaraSobreArtistaBtn">Aprovar</button>
+
+              <button className="sonaraSobreArtistaBtn">Contra Proposta</button>
+
+              <button className="sonaraSobreArtistaBtn">Salvar</button>
             </div>
           </div>
+        </div>
+      </main>
 
-          <div className="visualizarArtistaBotoes">
-            <button className="visualizarArtistaBotao">Reprovar</button>
-
-            <button className="visualizarArtistaBotao">Aprovar</button>
-
-            <button className="visualizarArtistaBotao">Contra Proposta</button>
-
-            <button className="visualizarArtistaBotao">Salvar</button>
-          </div>
-        </section>
-      </div>
-      <footer>
-        <FooterSonara />
-      </footer>
-    </div>
+      <FooterSonara />
+    </>
   );
 }
